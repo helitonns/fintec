cbb7758690a58e9eb99532359bfbefc8899fd8dc
heliton <helitonns@gmail.com>
2024-01-29 10:48:53 -0400

    Primeiro commit

---
+++ src/Context/DataContext.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useFetch from "../Hooks/useFetch";
+
+type IVenda = {
+  id: string;
+  nome: string;
+  preco: number;
+  status: "pagpo" | "processando" | "cartao";
+  pagamento: "boleto" | "pix" | "cartao";
+  data: string;
+  parcelas: number | null;
+}
+
+type IDataContext = {
+  data: IVenda[] | null;
+  loading: boolean;
+  error: string | null;
+}
+
+const DataContext = React.createContext<IDataContext | null>(null);
+
+export const useData = ()=> {
+  const context = React.useContext(DataContext);
+  if(!context) throw new Error("useData deve estar em DataContextProvider");
+  return context;
+}
+
+export const DataContextProvider = ({children}: React.PropsWithChildren)=> {
+  const {data, loading, error} = useFetch<IVenda[]>("https://data.origamid.dev/vendas");
+
+  return <DataContext.Provider value={{data, loading, error}}>{children}</DataContext.Provider>
+}
